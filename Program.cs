641baf86141acdbd6afd33e247c2a21e159a1b2f
Rodrigo Muñoz <rmunozdelaporte@gmail.com>
2019-09-17 22:13:10 -0300

    Dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
