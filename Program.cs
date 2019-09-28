fe12e008eeee876113935cc8667f826cbfd9c9e9
Rodrigo Muñoz <rmunozdelaporte@gmail.com>
2019-09-28 16:08:41 -0300

    Namespaces DRY

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 
-namespace Csharp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            System.String greeting = "Hello";   // string greeting
-            System.Int32 wholeNumber = 4;       // int wholeNumber
-            System.Double pi = 3.1415;          // double pi
-            System.Boolean status = true;       // bool status
-            System.Char letter = 'z';           // char letter
-
-            System.Console.WriteLine(greeting.GetType());
-            System.Console.WriteLine(wholeNumber.GetType());
-            System.Console.WriteLine(pi.GetType());
-            System.Console.WriteLine(status.GetType());
-            System.Console.WriteLine(letter.GetType());
-        }
+        Random generator = new Random();
+        double randomNumber = generator.NextDouble();
+        double roundedNumber = Math.Round(randomNumber, 1);
+        
+        Console.WriteLine(roundedNumber);
     }
 }
+
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+/* [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+public class UnitTestEquality
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+    public void TestEquality()
+    {
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(2, 2);
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(5, 5);
+    }
+} */
