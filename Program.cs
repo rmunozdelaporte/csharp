519818002ca205dd2ab249700bd005bb9725fcbe
Rodrigo Muñoz <rmunozdelaporte@gmail.com>
2019-09-28 13:10:34 -0300

    TypeOf Variables

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
-            System.Console.WriteLine("The current time is " + DateTime.Now);
+            System.String greeting = "Hello";   // string greeting
+            System.Int32 wholeNumber = 4;       // int wholeNumber
+            System.Double pi = 3.1415;          // double pi
+            System.Boolean status = true;       // bool status
+            System.Char letter = 'z';           // char letter
+
+            System.Console.WriteLine(greeting.GetType());
+            System.Console.WriteLine(wholeNumber.GetType());
+            System.Console.WriteLine(pi.GetType());
+            System.Console.WriteLine(status.GetType());
+            System.Console.WriteLine(letter.GetType());
         }
     }
 }
